52c7bd87606bdefbb635d5ba122d40d24bd47b08
Jon Stewart <jonathan.l.stewart@acm.org>
2013-08-02 12:02:50 -0400

    Incorporates bookmarking, numerous bug fixes (especially the CSV export flushing issue), and upgrades DataTables and jersey

---
+++ src/edu/utsa/sifter/BookmarkSearcher.java
@@ -0,0 +1,56 @@
+package edu.utsa.sifter;
+
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+
+import org.apache.lucene.document.Document;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class BookmarkSearcher {
+  final private IndexSearcher Searcher; 
+  final private Query SearchQuery;
+
+  private TopDocs Docs;
+
+  final private HashSet<String> FieldsToLoad = new HashSet<String>();
+
+  final private static int MAX_RESULTS = 1000;
+
+  BookmarkSearcher(final IndexSearcher s, final Query q) throws IOException {
+    Searcher = s;
+    SearchQuery = q;
+    Docs = Searcher.search(SearchQuery, MAX_RESULTS);
+
+    FieldsToLoad.add("Comment");
+    FieldsToLoad.add("Created");
+    FieldsToLoad.add("Docs");
+  }
+
+  public ArrayList<Bookmark> retrieve() throws IOException {
+    if (Docs.totalHits > 0) {
+      final ArrayList<Bookmark> list = new ArrayList();
+      for (int i = 0; i < Docs.totalHits; ++i) {
+        list.add(getResult(Docs.scoreDocs[i].doc));
+      }
+      return list;
+    }
+    else {
+      return null;
+    }
+  }
+
+  public Bookmark getResult(final int id) throws IOException {
+    final Document doc = Searcher.document(id, FieldsToLoad);
+    if (doc != null) {
+      return new Bookmark(doc);
+    }
+    return null;
+  }
+
+}
